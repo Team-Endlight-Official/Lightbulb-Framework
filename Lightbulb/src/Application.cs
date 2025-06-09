0fecf428f7130711d7ab7e5a8bc37dee9a6c3c6b
Team-Endlight-Official <team.endlight@gmail.com>
2025-06-09 20:40:26 +0200

    Add project files.

---
+++ Lightbulb/src/Application.cs
@@ -0,0 +1,117 @@
+﻿using Lightbulb.Graphics;
+using Microsoft.Xna.Framework;
+using System;
+
+namespace Lightbulb;
+
+/// <summary>
+/// The Main to go for all Games made in Ligthbulb/MonoGame.
+/// </summary>
+public class Application : Game
+{
+    // private members
+    private GraphicsDeviceManager m_GraphicsManager;
+
+    // public members
+
+    /// <summary>
+    /// Initialization of the Application. create, add and manage on startup your codes here. Do not load assets here.
+    /// Asset loading is for OnLoad.
+    /// </summary>
+    public Action OnInit;
+
+    /// <summary>
+    /// Method to load your resources.
+    /// </summary>
+    public Action OnLoad;
+
+    /// <summary>
+    /// Manage your GameLoop, Input and Logic in here. DO NOT use the OnDraw method to manage other stuff.
+    /// </summary>
+    public Action OnUpdate;
+
+    /// <summary>
+    /// Manage your Rendering.
+    /// </summary>
+    public Action OnDraw;
+
+    /// <summary>
+    /// Method that will be called whenever the Application closes/unloads. Use this to delete remaining resources to prevent leaking.
+    /// </summary>
+    public Action OnClose;
+
+    /// <summary>
+    /// Event that gets invoked whenever the Application is resized. Pretty convenient for certain calculations that happen only when resizing.
+    /// </summary>
+    public EventHandler<EventArgs> OnResize;
+
+    /// <summary>
+    /// The Main to go for all Games made in Ligthbulb/MonoGame.
+    /// </summary>
+    public Application()
+    {
+        m_GraphicsManager = new GraphicsDeviceManager(this);
+        Content.RootDirectory = "Content";
+        IsMouseVisible = true;
+    }
+
+
+    protected override void Initialize()
+    {
+        if (OnResize != null) Window.ClientSizeChanged += OnResize;
+        if (OnInit != null) OnInit();
+        base.Initialize();
+    }
+
+    protected override void LoadContent()
+    {
+        if (OnLoad != null) OnLoad();
+    }
+
+    protected override void Update(GameTime gameTime)
+    {
+        if (OnUpdate != null) OnUpdate();
+
+        base.Update(gameTime);
+    }
+
+    protected override void Draw(GameTime gameTime)
+    {
+        if (OnDraw != null) OnDraw();
+
+        base.Draw(gameTime);
+    }
+
+    protected override void UnloadContent()
+    {
+        if (OnClose != null) OnClose();
+        base.UnloadContent();
+    }
+
+    // public methods
+
+    public void SetSize(int width, int height)
+    {
+        Renderer.SetSize(width, height);
+    }
+
+    public void SetTitle(string title)
+    {
+        Window.Title = title;
+    }
+
+    public void SetFullscreen(bool fullscreen)
+    {
+        Renderer.SetFullscreen(fullscreen);
+    }
+
+    // public members
+
+    public GraphicsDeviceManager GraphicsDeviceManager
+    {
+        get
+        {
+            return m_GraphicsManager;
+        }
+    }
+}
---
+++ Lightbulb/src/Graphics/Renderer.cs
@@ -0,0 +1,143 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace Lightbulb.Graphics;
+
+/// <summary>
+/// The next Main to go for all Games made with Lightbulb/MonoGame. 
+/// The Renderer, this Renderer is an abstraction and cleanup that merges the Graphics Device, Graphics Device Manager and others into one stable and static codebase!
+/// </summary>
+public class Renderer
+{
+    private static bool m_Init = false;
+
+    // private static members
+    private static Application m_App;
+
+    // public static members
+
+    /// <summary>
+    /// The Graphics Device of the Application.
+    /// </summary>
+    public static GraphicsDevice Device { get; private set; }
+
+    /// <summary>
+    /// The Graphics Device Manager of the Application.
+    /// </summary>
+    public static GraphicsDeviceManager DeviceManager { get; private set; }
+
+    /// <summary>
+    /// The View Matrix for 3D calculations. Default testing view calculation is always made on Init.
+    /// </summary>
+    public static Matrix View;
+
+    /// <summary>
+    /// The Projection Matrix for 3D calculations. Default testing projection calculation is always made on Init.
+    /// </summary>
+    public static Matrix Projection;
+
+    /// <summary>
+    /// Gets the aspect ratio of the Application's Viewport.
+    /// </summary>
+    public static float AspectRatio
+    {
+        get
+        {
+            return Device.Viewport.AspectRatio;
+        }
+    }
+
+    /// <summary>
+    /// The Application's Viewport. can be changed directly.
+    /// </summary>
+    public static Viewport Viewport
+    {
+        get
+        {
+            return Device.Viewport;
+        }
+        set
+        {
+            Device.Viewport = value;
+        }
+    }
+
+
+    // public static defaults
+
+    /// <summary>
+    /// The Default clearing options on "Renderer.Clear();"
+    /// </summary>
+    public static readonly ClearOptions DefaultClearOptions = (ClearOptions.Target | ClearOptions.DepthBuffer);
+
+    /// <summary>
+    /// The Default clearing depth.
+    /// </summary>
+    public static readonly float DefaultDepth = 1.0f;
+
+    /// <summary>
+    /// The Default clearing stencil.
+    /// </summary>
+    public static readonly int DefaultStencil = 0;
+
+    
+    // public static methods
+
+    /// <summary>
+    /// Initializes the Renderer's subsystem. ALWAYS CALL ONCE IN YOUR OnLoad METHOD!
+    /// </summary>
+    /// <param name="application">Your target application.</param>
+    public static void Init(Application application)
+    {
+        if (m_Init) return;
+
+        m_App = application;
+        Device = m_App.GraphicsDevice;
+        DeviceManager = m_App.GraphicsDeviceManager;
+
+        View = Matrix.CreateLookAt(new Vector3(0f, 0f, 5f), Vector3.Zero, Vector3.Up);
+        Projection = Matrix.CreatePerspectiveFieldOfView(MathHelper.ToRadians(45f), AspectRatio, 0.1f, 1000f);
+
+        m_Init = true;
+    }
+
+    /// <summary>
+    /// Clears the graphics, making it ready to draw.
+    /// </summary>
+    public static void Clear()
+    {
+        Device.Clear(DefaultClearOptions, Color.Black, DefaultDepth, DefaultStencil);
+    }
+
+    /// <summary>
+    /// Clears the graphics, making it ready to draw.
+    /// </summary>
+    /// <param name="clearColor">Your target clearing color.</param>
+    public static void Clear(Color clearColor)
+    {
+        Device.Clear(DefaultClearOptions, clearColor, DefaultDepth, DefaultStencil);
+    }
+
+
+    /// <summary>
+    /// Sets the size of your Application and the Graphical Context within.
+    /// </summary>
+    /// <param name="width">Your target width.</param>
+    /// <param name="height">Your target height.</param>
+    public static void SetSize(int width, int height)
+    {
+        DeviceManager.PreferredBackBufferWidth = width;
+        DeviceManager.PreferredBackBufferHeight = height;
+        DeviceManager.ApplyChanges();
+    }
+
+    /// <summary>
+    /// Sets the fullscreen of your Application and the Graphical Context within.
+    /// </summary>
+    /// <param name="fullscreen">Your target fullscreen value.</param>
+    public static void SetFullscreen(bool fullscreen)
+    {
+        DeviceManager.IsFullScreen = fullscreen;
+        DeviceManager.ApplyChanges();
+    }
+}

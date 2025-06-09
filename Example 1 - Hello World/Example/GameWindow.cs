e4374c4b2f262712cd6babf1cb4c853e879c58f0
Team-Endlight-Official <team.endlight@gmail.com>
2025-06-09 20:51:59 +0200

    Added and Tested the first Example using the build class library of the framework :D

---
+++ Example 1 - Hello World/Example/GameWindow.cs
@@ -0,0 +1,54 @@
+﻿using Lightbulb;
+using Lightbulb.Graphics;
+using Microsoft.Xna.Framework;
+using System;
+
+namespace Endlight.Example;
+
+public class GameWindow
+{
+    int width = 800;
+    int height = 600;
+    string title = "Example 1. Hello World";
+
+    Application application;
+
+    public GameWindow()
+    {
+        application = new Application();
+        application.OnInit += Init;
+        application.OnLoad += Load;
+        application.OnUpdate += Update;
+        application.OnDraw += Draw;
+        application.OnClose += Unload;
+
+        application.Run();
+    }
+
+    void Init()
+    {
+        Renderer.Init(application);
+        application.SetSize(width, height);
+        application.SetTitle(title);
+    }
+
+    void Load()
+    {
+        Console.WriteLine("Hello my Window!");
+    }
+
+    void Update()
+    {
+
+    }
+
+    void Draw()
+    {
+        Renderer.Clear(Color.Blue);
+    }
+
+    void Unload()
+    {
+        Console.WriteLine("Goodbye my Window!");
+    }
+}
---
+++ Example 1 - Hello World/Example/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Endlight.Example;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, Example 1");
+        GameWindow game = new GameWindow();
+    }
+}
